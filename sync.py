f6b1e784c4b1af106178a9590d5f04e379529b41
Stanislav Sutulo <36450612+ktago336@users.noreply.github.com>
2022-05-23 23:44:44 +0300

    Add files via upload

--- sync.py
+++ sync.py
@@ -2,54 +2,86 @@ import sys
 import socket
 import time
 import os
+import setupsync as start
 
-ip=sys.argv[1]
-port=5555
+path=os.getcwd()+'/files'
+destinationIP=sys.argv[1]
+Port=9090
 sendOrRecv=sys.argv[2]
-    
+if sendOrRecv=='s':
+    dataToSend=sys.argv[3]
+
+start.firstRun()
+
+path='./files'
+nOfFiles=len([f for f in os.listdir(path)
+              if os.path.isfile(os.path.join(path,f))])
+print(nOfFiles)
+
+
+#if recieving
+if sendOrRecv=='0':
+    print('reading mode on')
+    sock=socket.socket()
+    sock.bind(('', Port))
+    print('listening...')
+    sock.listen(1)
+    conn, addr=sock.accept()
+
+    print('Connection established ', addr)
+
+    f=open(dataToSend,'w')
+    f.close()
+    f=open(dataToSend,'a')
+    while True:
+        data=conn.recv(2048)
+        if not data:
+            break
+        f.write(data)
+    conn.close()
+    f.close()
+    print('OK')
 if sendOrRecv=='s':
     print ('send mode')
     sock=socket.socket()
-    sock.connect((ip, port))
+    sock.connect(('10.0.0.69', 5555))
 
-    f_name=input('data to send\n')
-    sock.send((bytes(f_name,encoding='UTF-8')))
-    time.sleep(0.1)
-    f=open(f_name,  "rb")
-    l=f.read(1024)
-    while (l):
-        sock.sendall(l)
-        l=f.read(1024)
-    sock.close()
-    f.close()
+    f_name=input('data to send')
+    sock.send(())
     print ('sucseed')
     
 if sendOrRecv=='r':
     
     sock = socket.socket()
+    ip = "10.0.0.24"
+    port = 22
     sock.bind((ip, port))
     print('listening...', sock.getsockname())
     sock.listen(10)
+    print('CONNECTED')
 
     while True:
         conn, addr = sock.accept()
+
         print('connected to:', addr)
-        
+
         name_f = (conn.recv(1024)).decode ('UTF-8')
-        f = open(name_f,'wb')
+
+        f = open('files/' + name_f,'wb')
 
         while True:
+
             l = conn.recv(1024)
+
             f.write(l)
-            print('1024 bytes writed...')
+
             if not l:
-                print('breaking...')
                 break
-            
+
         f.close()
         conn.close()
-    print ('sucseed')
-    print('File received')
+print ('sucseed')
+print('File received')
 
 sock.close()
     
